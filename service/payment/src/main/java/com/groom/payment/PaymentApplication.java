68343b33747ee5da5f89682d431ed990f082f0c0
boxty <boxty@boxtyui-MacBookPro.local>
2026-01-25 13:51:59 +0900

    fix: 모든 도메인 컴파일 에러 수정

--- service/payment/src/main/java/com/groom/payment/PaymentApplication.java
+++ service/payment/src/main/java/com/groom/payment/PaymentApplication.java
@@ -1,11 +1,14 @@
 package com.groom.payment;
 
+import com.groom.payment.infrastructure.config.TossPaymentsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.groom.payment", "com.groom.common"})
 @EnableFeignClients
+@EnableConfigurationProperties(TossPaymentsProperties.class)
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
