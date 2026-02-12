750fe35f3eb37faa365edf1388a22f94aeb8ab12
boxty <boxty@boxtyui-MacBookPro.local>
2026-02-12 14:43:26 +0900

    cart jwt제거 및 전용 SecurityConfig생성

--- service/cart/src/main/java/com/groom/cart/CartApplication.java
+++ service/cart/src/main/java/com/groom/cart/CartApplication.java
@@ -2,11 +2,23 @@ package com.groom.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"com.groom.cart", "com.groom.common"})
+@SpringBootApplication(
+        scanBasePackages = {"com.groom.cart", "com.groom.common"},
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        },
+        excludeName = {
+                "com.groom.common.infrastructure.config.security.JwtSecurityConfig"
+        }
+)
 @EnableFeignClients
 public class CartApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
     }
