59f2caa47d7c1d3ce2c402aa9b1f8dc3879a4011
yseoy0311 <yseoy0311@gmail.com>
2026-02-16 21:54:28 +0900

    feat: request mapping

--- service/order/src/main/java/com/groom/order/presentation/controller/HealthController.java
+++ service/order/src/main/java/com/groom/order/presentation/controller/HealthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v2/orders")
 public class HealthController {
 
 	@GetMapping("/health")
